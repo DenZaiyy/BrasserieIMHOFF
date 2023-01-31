9bb8ef9e063f5b8612fd23082eae5701e7ae5cf2
DenZaiyy <denzaiyy@outlook.fr>
2023-01-31 12:02:14 +0100

    Change on pause

---
+++ script/index.js
@@ -0,0 +1,37 @@
+function getMinPrice1() {
+    document.getElementById('price-1').innerHTML = "2,50 €"
+    document.getElementById('beer-min-1').classList.add('item-select')
+    document.getElementById('beer-max-1').classList.remove('item-select')
+}
+
+function getMaxPrice1() {
+    document.getElementById('price-1').innerHTML = "5 €"
+    document.getElementById('beer-max-1').classList.add('item-select')
+    document.getElementById('beer-min-1').classList.remove('item-select')
+
+}
+
+function getMinPrice2() {
+    document.getElementById('price-2').innerHTML = "2,50 €"
+    document.getElementById('beer-min-2').classList.add('item-select')
+    document.getElementById('beer-max-2').classList.remove('item-select')
+}
+
+function getMaxPrice2() {
+    document.getElementById('price-2').innerHTML = "5 €"
+    document.getElementById('beer-max-2').classList.add('item-select')
+    document.getElementById('beer-min-2').classList.remove('item-select')
+
+}
+function getMinPrice3() {
+    document.getElementById('price-3').innerHTML = "2,50 €"
+    document.getElementById('beer-min-3').classList.add('item-select')
+    document.getElementById('beer-max-3').classList.remove('item-select')
+}
+
+function getMaxPrice3() {
+    document.getElementById('price-3').innerHTML = "5 €"
+    document.getElementById('beer-max-3').classList.add('item-select')
+    document.getElementById('beer-min-3').classList.remove('item-select')
+
+}
